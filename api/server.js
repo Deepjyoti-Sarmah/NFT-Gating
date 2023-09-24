82bf9b1ac97b7b394120bcd6e0d8d734150dab34
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-09-24 16:21:47 +0530

    adding nodemon

--- api/server.js
+++ api/server.js
@@ -1,2 +1,9 @@
 const express = require("express");
 const {Web3} = require("web3"); 
+const ABI = require("./ABI.json");
+
+const app = express();
+
+add.listen(3000,() => {
+  console.log(`Server is running at ${PORT}`)
+})
