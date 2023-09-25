d394c8353aa9a9a8fdab7dd5dd40ec224e083572
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-09-25 15:01:21 +0530

    fetching the NFTs

--- api/server.js
+++ api/server.js
@@ -3,9 +3,26 @@ const {Web3} = require("web3");
 const ABI = require("./ABI.json");
 
 const app = express();
+const web3 = new Web3("https://skilled-wiser-lake.ethereum-sepolia.discover.quiknode.pro/73477a64596690dd501ed8f4cd73113918f4800a/");
+const contractAddress = "0x1C5008E8853D89E6bD823A72FCB6512fA1b74EC8";
+
+const contract = new web3.eth.Contract(ABI, contractAddress);
+// console.log(contract);
+
+const fetchNFTs = async() => {
+  try {
+    const nftBalance = await contract.methods.balanceOf("0x18992684FBeEEd5A61B48610fec6137a924cBC98").call();
+    return {userNFTs:nftBalance};
+  } catch (error) {
+    console.error("Error in fetching NFT");
+  }
+  // console.log(Number(nftBalance));
+}
+fetchNFTs().then(console.log);
+
+
 
 const PORT = 3000;
-const web3 = new Web3("")
 
 app.listen(PORT,() => {
   console.log(`Server is running at ${PORT}`)
