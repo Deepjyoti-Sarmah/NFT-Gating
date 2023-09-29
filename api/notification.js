49e1056be7d725a26976f3fa98874ba5330dfdbc
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-09-29 19:34:05 +0530

    adding notification but getting error

--- api/notification.js
+++ api/notification.js
@@ -7,9 +7,9 @@ const headers = {
 
 const data = {
   name: 'NFT Transfer',
-  expression: '',
+  expression: 'KHR4X2xvZ3NfdG9waWMxID1+ICcxODk5MjY4NEZCZUVFZDVBNjFCNDg2MTBmZWM2MTM3YTkyNGNCQzk4JykgJiYgCih0eF9sb2dzX2FkZHJlc3MgPT0gJzB4N2ZiMGYzYzM0NzA1NmQyZDcxNDFmNTlmMjY0ODRlM2EzNWI1MjhjYScpICYmIAoodHhfbG9nc190b3BpYzAgPT0gJzB4ZDcyNTNlMzhjNmUzYWU0ZDM0MWVlOWU0ODI0YzY5YzFjMDYwZWQ2N2EzMTI2NDJhYzU4MmIyNmE1NTVhNjExYycpCgo=',
   network: 'ethereum-sepolia',
-  destinationIds: ['https://50cf-2409-408a-8215-a35e-1175-54f2-20bb-12b3.ngrok-free.app 2cb3d4a2-adc7-4416-ac07-2a8962315476']
+  destinationIds: ['b18806cd-5f47-4ee2-8ee8-338b03208656']
 };
 
 axios.post('https://api.quicknode.com/quickalerts/rest/v1/notifications', data, { headers })
@@ -19,3 +19,4 @@ axios.post('https://api.quicknode.com/quickalerts/rest/v1/notifications', data, 
 // (tx_logs_topic1 =~ '18992684FBeEEd5A61B48610fec6137a924cBC98') && 
 // (tx_logs_address == '0x7fb0f3c347056d2d7141f59f26484e3a35b528ca') && 
 // (tx_logs_topic0 == '0xd7253e38c6e3ae4d341ee9e4824c69c1c060ed67a312642ac582b26a555a611c')
+//
