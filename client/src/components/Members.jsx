ae9a9850a250b035ece4b61edc9cd4f250a7ec8d
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-10-01 12:52:48 +0530

    setting socketio in client side

--- client/src/components/Members.jsx
+++ client/src/components/Members.jsx
@@ -1,9 +1,24 @@
 import welcomeImg  from "../images/index.jpeg"
+import io from "socket.io-client";
+import { useState, useEffect} from "react";
+
 import React from 'react'
 
 const Members = () => {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(()=> {
+    const socketInstance= io('http://localhost:3000');
+    setSocket(socketInstance);
+    console.log(socket);
+
+    return() => {
+      socketInstance.disconnect();
+    }
+  }, [socket]);
   return (
     <>
+      
       <p>Thank you for checking out</p>
       <img src={welcomeImg} />
     </>
